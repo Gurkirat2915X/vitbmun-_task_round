be1945072fe1328fd88a4d90621175f1d89cd6d1
Gurkirat Singh Dhillon <navilavi.kol123@gmail.com>
2024-11-30 23:30:34 +0530

    Formatting of code

--- README.md
+++ README.md
@@ -1,37 +1,45 @@
-# vitbmun-_task_round
+# vitbmun-\_task_round
+
 ## Project published at : https://gurkirat2915x.github.io/vitbmun_task_round/
+
 ## Table of Contents
+
 - [Introduction](#introduction)
 - [Features](#features)
 - [Installation](#installation)
 - [Usage](#usage)
 
-
 ## Introduction
+
 As per the requirements outlined in the task for the technical round for **VIT-B MUN Club**, this website has been developed using HTML, CSS, JavaScript, and the [Swiper](https://swiperjs.com/) library. Frameworks like Bootstrap were intentionally not used to showcase my frontend development skills.
 
 The design and features of this website are inspired by the [VIT-B MUN website](https://vitbmun.netlify.app/).
 
 ## Features
+
 - **User-Friendly Responsive Interface:** Ensures seamless navigation across all devices.
-- **Glass-morphic navbar:**  Designed specifically for mobile and tablet users to enhance visual appeal.
+- **Glass-morphic navbar:** Designed specifically for mobile and tablet users to enhance visual appeal.
 - **Fully Responsive Design:** Supports all screen sizes and types for optimal user experience.
 - **Framework-Free Animations:** Created using plain HTML, CSS, and JavaScript to demonstrate pure development skills.
 - **Wave-Animated Footer:** Adds a dynamic and modern touch to the design.
-## Installation
-To install this project, follow these steps:
-1. Clone the repository:
-    ```bash
-    git clone https://github.com/gurkirat2915x/vitbmun_task_round.git
-    ```
-2. Navigate to the project directory:
-    ```bash
-    cd vitbmun_task_round
-    ```
 
+## Installation
+
+To install this project, follow these steps:
+
+1. Clone the repository:
+   ```bash
+   git clone https://github.com/gurkirat2915x/vitbmun_task_round.git
+   ```
+2. Navigate to the project directory:
+   ```bash
+   cd vitbmun_task_round
+   ```
 
 ## Usage
+
 To start the project, run the following command:
+
 ```bash
 .\index.html
 ```
--- script.js
+++ script.js
@@ -1,46 +1,45 @@
-const toggle_Btn = document.querySelector('.toggle_btn');
-const toggle_Btn_Icon = document.querySelector('.toggle_btn i');
-const dropdown_menu = document.querySelector('.dropdown_menu');
-toggle_Btn.addEventListener('click', () => {
-    dropdown_menu.classList.toggle('active');
-    const isActive = dropdown_menu.classList.contains('active');
+// This is the main script file for the website
+// this is toggle button for dropdown menu
+// It will toggle the dropdown menu when clicked
+const toggle_Btn = document.querySelector(".toggle_btn");
+const toggle_Btn_Icon = document.querySelector(".toggle_btn i");
+const dropdown_menu = document.querySelector(".dropdown_menu");
+toggle_Btn.addEventListener("click", () => {
+  dropdown_menu.classList.toggle("active");
+  const isActive = dropdown_menu.classList.contains("active");
 
-    toggle_Btn_Icon.classList = isActive? 'fas fa-times' : 'fas fa-bars';
-    });
-new Swiper('.card-wrapper', {
-        // Optional parameters
-        // direction: 'vertical',
+  toggle_Btn_Icon.classList = isActive ? "fas fa-times" : "fas fa-bars";
+});
+// card swiper
+new Swiper(".card-wrapper", {
+  loop: true,
 
-        loop: true,
-      
-        // If we need pagination
-        pagination: {
-          el: '.swiper-pagination',
-          clickable: true,
-          dynamicBullets: true,
-        },
-      
-        // Navigation arrows
-        navigation: {
-          nextEl: '.swiper-button-next',
-          prevEl: '.swiper-button-prev',
-        },
-      
-        // And if we need scrollbar
-        breakpoints:
-        {
-            0:{
-                slidesPerView: 1,
-                spaceBetween: 10,
-            },
-            576:{
-                slidesPerView: 2,
-                spaceBetween: 20,
-            },
-            992:{
-                slidesPerView: 3,
-                spaceBetween: 30,
-            }
-        }
-      });
+  // If we need pagination
+  pagination: {
+    el: ".swiper-pagination",
+    clickable: true,
+    dynamicBullets: true,
+  },
 
+  // Navigation arrows
+  navigation: {
+    nextEl: ".swiper-button-next",
+    prevEl: ".swiper-button-prev",
+  },
+
+  // breakpoints
+  breakpoints: {
+    0: {
+      slidesPerView: 1,
+      spaceBetween: 10,
+    },
+    576: {
+      slidesPerView: 2,
+      spaceBetween: 20,
+    },
+    992: {
+      slidesPerView: 3,
+      spaceBetween: 30,
+    },
+  },
+});
